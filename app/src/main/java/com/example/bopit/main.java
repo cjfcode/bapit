1cf3c79e31013d56b2bb578d93a8a6e4dfa4091a
cjfcode <inbox.chrisf@gmail.com>
2021-09-23 07:14:44 -0400

    updated SDK versions

--- app/src/main/java/com/example/bopit/main.java
+++ app/src/main/java/com/example/bopit/main.java
@@ -12,8 +12,8 @@ package com.example.bopit;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
